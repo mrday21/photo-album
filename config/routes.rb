f587838ad5a6c3ad957b1ba7226106a749d1feb5
Brian Day <daycoderindustries@gmail.com>
2019-02-18 02:21:19 +0000

    Added sessions controller and login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
        get 'photos', to: 'pages#photos'
    resources :users 
        get 'signup'  => 'users#new' 
-
+       get '/login' => 'sessions#new'
        get 'albums' => 'albums#index'
        get 'albums/new' => 'albums#new'
        get 'albums/:id' => 'albums#show', as: :album
